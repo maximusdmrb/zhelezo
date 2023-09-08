c70b04c8ec7f01ac9f63fa75c789704737b42431
Максим Стельмашенко <maximusdmrb@gmail.com>
2023-09-08 13:28:59 +0300

    modified for gh-pages

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(3333, () => console.log("App started"));
